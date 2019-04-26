cfe6e9bc8e8f5ef4ee9ad2b05ce13f32e878354d
Sasha Ames <amysash2006@gmail.com>
2019-04-26 15:23:54 -0700

    in prog update to new schema and interface changes

--- cog/views/views_subscription.py
+++ cog/views/views_subscription.py
@@ -18,18 +18,36 @@ from cog.views.utils import getQueryDict
 
 from cog.plugins.esgf.security import esgfDatabaseManager
 
+
+def lookup_and_render(request):
+
+	email = request.user.email
+
+	dbres = esgfDatabaseManager.getUserSubscriptions(email)
+
+	render(request, 'cog/subscription/subscribe_list.html', dbres)
+
 @login_required
 def subscribe(request):
 
 	if request.method=='GET':
 
-		return render(request, 'cog/subscription/subscribe.html')
+		if request.GET.get('action') == "modify":
+			return lookup_and_render(request)
+		else:	
+			return render(request, 'cog/subscription/subscribe.html')
 	else:
+	# result.inserted_primary_key
+
+		if 
+
 		email = request.user.email
 
 		subs_count = 0
 
 		error_cond = ""
+		keyarr = []
+		valarr = []
 		for i in range(1,4):
 
 			keystr = 'subscription_key{}'.format(i)
@@ -43,16 +61,24 @@ def subscribe(request):
 			if len(keyres) < 2 or len(valres) < 2:
 				continue
 
+			keyarr.append(keyres)
+			valarr.append(valres)
+
+			subs_count = subs_count + 1
+
+		if subs_count > 0:
+
 			try:
-				esgfDatabaseManager.addUserSubscription(email, keyres, valres )
+				
+				esgfDatabaseManager.addUserSubscription(email, keyarr, valarr )
+
 			except Exception as e:
 				# log error
 				error_cond = str(e)
 				return render(request, 'cog/subscription/subscribe_done.html', { 'email' : email ,  'error' : "An Error Has Occurred While Processing Your Request. <p> {}".format(error_cond), })
 
-			subs_count = subs_count + 1
 
-		if subs_count > 0:
+
 			return render(request, 'cog/subscription/subscribe_done.html', { 'email' : email , 'count' : subs_count })
 		else:
 			return render(request, 'cog/subscription/subscribe.html')			
