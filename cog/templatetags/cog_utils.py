9d048cd832995a783f59c00e035a9d066ced8ff6
Carlos Downie <42552189+downiec@users.noreply.github.com>
2019-08-01 16:10:23 -0700

    Experimental adding of react components successful. Added a reactstrap dropdown with items rendered inside

--- cog/templatetags/cog_utils.py
+++ cog/templatetags/cog_utils.py
@@ -22,8 +22,25 @@ import urlparse
 import string
 import bleach
 from cog.views.utils import getKnownIdentityProviders, getQueryDict, paginate
+import json as jsonlib
 
 register = template.Library()
+
+@register.filter
+def json(value):
+    """safe jsonify filter, bleaches the json string using the bleach html tag remover"""
+    uncleaned = jsonlib.dumps(value)
+    clean = bleach.clean(uncleaned)
+
+    try:
+        jsonlib.loads(clean)
+    except:
+        # should never happen, but this is a last-line-of-defense check
+        # to make sure this blob wont get eval'ed by the JS engine as 
+        # anything other than a JSON object
+        raise ValueError('JSON contains a quote or escape sequence that was unable to be stripped')
+
+    return mark_safe(clean)
 
 @register.filter
 def wps_arguments(the_dict):
--- cog/views/views_subscription.py
+++ cog/views/views_subscription.py
@@ -15,94 +15,113 @@ from cog.plugins.esgf.security import esgfDatabaseManager
 
 import traceback
 
+# Code used for react components
+
+# Get static js files list
+js_files = os.listdir("/Users/downie4/Desktop/COG_devel/COG/cog/static/cog/cog-react/js")
+css_files = os.listdir("/Users/downie4/Desktop/COG_devel/COG/cog/static/cog/cog-react/css")
+js_files = list(map(lambda f: "cog/cog-react/js/" + f, js_files))
+css_files = list(map(lambda f: "cog/cog-react/css/" + f, css_files))
+
+print(js_files)
+
+react_files = {
+    'css': css_files,
+    'js': js_files
+}
+
+react_props = {
+    'name': "COG",
+    'enthusiasmLevel': 7,
+    'items': ["test1","test2","test3","test4","test5"]
+}
+
+
 def lookup_and_render(request):
 
-	email = request.user.email
+    email = request.user.email
 
-	try:
-		dbres = esgfDatabaseManager.lookupUserSubscriptions(email)
-	except Exception as e:
-		# log error
-		error_cond = str(e)
-		print traceback.print_exc()
-		return render(request, 'cog/subscription/subscribe_done.html', { 'email' : email ,  'error' : "An Error Has Occurred While Processing Your Request. <p> {}".format(error_cond) })
-	
-	return render(request, 'cog/subscription/subscribe_list.html', { 'dbres' : dbres } )
+    try:
+        dbres = esgfDatabaseManager.lookupUserSubscriptions(email)
+    except Exception as e:
+        # log error
+        error_cond = str(e)
+        print traceback.print_exc()
+        return render(request, 'cog/subscription/subscribe_done.html', {'email': email,  'error': "An Error Has Occurred While Processing Your Request. <p> {}".format(error_cond)})
+
+    return render(request, 'cog/subscription/subscribe_list.html', {'dbres': dbres})
+
 
 def delete_subscription(request):
 
-	res = request.POST.get('subscription_id', None)
-	try:
-		if res == "ALL":
-			email = request.user.email
-			dbres = esgfDatabaseManager.deleteAllUserSubscriptions(email)
-		else:
-			dbres = esgfDatabaseManager.deleteUserSubscriptionById(res)
-	except Exception as e:
-		# log error
-		error_cond = str(e)
-		return render(request, 'cog/subscription/subscribe_done.html', { 'error' : "An Error Has Occurred While Processing Your Request. <p> {}".format(error_cond) })
+    res = request.POST.get('subscription_id', None)
+    try:
+        if res == "ALL":
+            email = request.user.email
+            dbres = esgfDatabaseManager.deleteAllUserSubscriptions(email)
+        else:
+            dbres = esgfDatabaseManager.deleteUserSubscriptionById(res)
+    except Exception as e:
+        # log error
+        error_cond = str(e)
+        return render(request, 'cog/subscription/subscribe_done.html', {'error': "An Error Has Occurred While Processing Your Request. <p> {}".format(error_cond)})
 
-	return render(request, 'cog/subscription/subs_delete_done.html')
-
+    return render(request, 'cog/subscription/subs_delete_done.html')
 
 @login_required
 def subscribe(request):
 
-	if request.method=='GET':
+    if request.method == 'GET':
 
-		if request.GET.get('action') == "modify":
-			return lookup_and_render(request)
-		else:	
-			return render(request, 'cog/subscription/subscribe.html')
-	elif request.POST.get('action') == "delete":
-		return delete_subscription(request)
-	else:
-	# result.inserted_primary_key
+        if request.GET.get('action') == "modify":
+            return lookup_and_render(request)
+        else:
+            return render(request, 'cog/subscription/subscribe.html', {'react_files': react_files, 'react_props': react_props})
+    elif request.POST.get('action') == "delete":
+        return delete_subscription(request)
+    else:
+        # result.inserted_primary_key
 
+        email = request.user.email
 
-		email = request.user.email
+        period = request.POST.get("period", -1)
+        if period == -1:
+            return render(request, 'cog/subscription/subscribe_done.html', {'email': email,  'error': "Invalid period"})
 
-		period = request.POST.get("period", -1)
-		if period == -1:
-			return render(request, 'cog/subscription/subscribe_done.html', { 'email' : email ,  'error' : "Invalid period" })
+        subs_count = 0
+        error_cond = ""
+        keyarr = []
+        valarr = []
+        for i in range(1, 4):
 
-		subs_count = 0
-		error_cond = ""
-		keyarr = []
-		valarr = []
-		for i in range(1,4):
+            keystr = 'subscription_key{}'.format(i)
+            keyres = request.POST.get(keystr, '')
 
-			keystr = 'subscription_key{}'.format(i)
-			keyres = request.POST.get(keystr, '')
-
-			valstr = 'subscription_value{}'.format(i)
-			valres = request.POST.get(valstr, '')
+            valstr = 'subscription_value{}'.format(i)
+            valres = request.POST.get(valstr, '')
 
 #			print "{},{},{},{},{}".format(i,keystr, valstr, keyres, valres)
 
-			if len(keyres) < 2 or len(valres) < 2:
-				continue
+            if len(keyres) < 2 or len(valres) < 2:
+                continue
 
-			keyarr.append(keyres)
-			valarr.append(valres)
+            keyarr.append(keyres)
+            valarr.append(valres)
 
-			subs_count = subs_count + 1
+            subs_count = subs_count + 1
 
-		if subs_count > 0:
+        if subs_count > 0:
 
-			try:
-				
-				esgfDatabaseManager.addUserSubscription(email, period, keyarr, valarr )
+            try:
 
-			except Exception as e:
-				# log error
-				error_cond = str(e)
-				return render(request, 'cog/subscription/subscribe_done.html', { 'email' : email ,  'error' : "An Error Has Occurred While Processing Your Request. <p> {}".format(error_cond), })
+                esgfDatabaseManager.addUserSubscription(
+                    email, period, keyarr, valarr)
 
+            except Exception as e:
+                # log error
+                error_cond = str(e)
+                return render(request, 'cog/subscription/subscribe_done.html', {'email': email,  'error': "An Error Has Occurred While Processing Your Request. <p> {}".format(error_cond), })
 
-
-			return render(request, 'cog/subscription/subscribe_done.html', { 'email' : email , 'count' : subs_count })
-		else:
-			return render(request, 'cog/subscription/subscribe.html')			
-	
+            return render(request, 'cog/subscription/subscribe_done.html', {'email': email, 'count': subs_count})
+        else:
+            return render(request, 'cog/subscription/subscribe.html', {'react_files': react_files, 'react_props': react_props})
