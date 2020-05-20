e6877d2246186accda6c8bdf7d527d99c33521b0
root <root@esgf-node.llnl.gov>
2020-05-20 11:51:23 -0700

    custom change for llnl

--- apache/wsgi.py
+++ apache/wsgi.py
@@ -18,7 +18,7 @@ import signal
 import sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, '/usr/local/cog/cog_install')
+sys.path.insert(0, '/usr/local/cog/cog_py3_install')
 
 os.environ["COG_CONFIG_DIR"] = "/usr/local/cog/cog_config"
 os.environ['HTTPS'] = "on" # instructs Django to prepend 'https' to fully generated links
